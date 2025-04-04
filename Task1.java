e87c07e5cb03d7565c31680df91634833be9b788
Apoorvi <164733234+apoorvi-s@users.noreply.github.com>
2025-04-04 10:04:46 +0530

    Update and rename GuessGame.java to Task1.java

--- Task1.java
+++ Task1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class GuessGame {
+public class Task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
